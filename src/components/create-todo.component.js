9ebbd1dbedebdaa05a8f737bcffc0de1dafc53d0
nivedmk <43902334+nivedmk@users.noreply.github.com>
2021-02-04 18:43:59 +0530

    Final Commit front-end

---
+++ src/components/create-todo.component.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+
+export default class CreateTodo extends Component {
+  defaultState = {
+    todo_description: "",
+    todo_responsible: "",
+    todo_priority: "",
+    todo_completed: false,
+  };
+
+  constructor(props) {
+    super(props);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeResponsible = this.onChangeResponsible.bind(this);
+    this.onChangePriority = this.onChangePriority.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = this.defaultState;
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      todo_description: e.target.value,
+    });
+  }
+
+  onChangeResponsible(e) {
+    this.setState({
+      todo_responsible: e.target.value,
+    });
+  }
+
+  onChangePriority(e) {
+    this.setState({
+      todo_priority: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(
+      "Form Submitted" +
+        "\n" +
+        this.state.todo_completed +
+        "\n" +
+        this.state.todo_description +
+        "\n" +
+        this.state.todo_priority +
+        "\n" +
+        this.state.todo_responsible +
+        "\n"
+    );
+    this.setState(this.defaultState);
+  }
+
+  render() {
+    return (
+      <div style={{ marginTop: 20 }}>
+        <h3>CreateNew ToDo</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Description:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.todo_description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Responsible:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.todo_responsible}
+              onChange={this.onChangeResponsible}
+            />
+          </div>
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityLow"
+                value="Low"
+                checked={this.state.todo_priority === "Low"}
+                onChange={this.onChangePriority}
+              />
+              <label className="fomr-check-label">Low</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityMedium"
+                value="Medium"
+                checked={this.state.todo_priority === "Medium"}
+                onChange={this.onChangePriority}
+              />
+              <label className="fomr-check-label">Medium</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityHigh"
+                value="High"
+                checked={this.state.todo_priority === "High"}
+                onChange={this.onChangePriority}
+              />
+              <label className="fomr-check-label">High</label>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create ToDo"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditTodo extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcom to edit Todo </p>
+            </div>
+        )
+    }
+}
